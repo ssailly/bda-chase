860d6bcc5a0207d8ac421c680bafc24083bba7f3
Line Rabehanta <line.rabehanta@gmail.com>
2023-05-18 18:07:24 +0200

    Oubli Chase.java

--- src/Chase.java
+++ src/Chase.java
@@ -89,6 +89,7 @@ public class Chase {
             query = query.substring(0, query.length() - 5); // remove last AND
         }
         query += ";";
+
         try (Statement statement = conn.createStatement();
              ResultSet res = statement.executeQuery(query)) {
             while (res.next()) {
@@ -213,8 +214,6 @@ public class Chase {
     }
 
     private static void egalize(Connection conn, Dependency.EqualityAtom atom, String phiCondition) {
-        System.out.println("PHI CONDITION = "+phiCondition);
-
         /**
          * T1 et T2 les tables de l'atome d'egalite
          *
@@ -244,18 +243,18 @@ public class Chase {
         else {
             updateTable(
                     conn,
-                    atom.table1.getName(),
                     atom.table2.getName(),
-                    atom.nomCol1,
+                    atom.table1.getName(),
                     atom.nomCol2,
+                    atom.nomCol1,
                     phiCondition
             );
             updateTable(
                     conn,
-                    atom.table2.getName(),
                     atom.table1.getName(),
-                    atom.nomCol2,
+                    atom.table2.getName(),
                     atom.nomCol1,
+                    atom.nomCol2,
                     phiCondition
             );
         }
@@ -269,7 +268,22 @@ public class Chase {
                 + " AND (" + refTable + "." + refCol + "<>" + tableToUpdate+ "." + colToUpdate
                 + " OR " + tableToUpdate+ "." + colToUpdate + " IS NULL)"
                 + " AND " + refTable + "." + refCol + " IS NOT NULL;";
-        System.out.println("update query -> " + updateQuery);
+        System.out.println("Mise a jour ->\n\t" + updateQuery);
+
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(updateQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void updateTableWithConstant(Connection conn, String tableToUpdate, String colToUpdate, String constant, String wherePart) {
+        String updateQuery =
+                "UPDATE " + tableToUpdate
+                        + " SET " + colToUpdate + " = '" + constant + "'"
+                        + " " + wherePart
+                        + " AND (" + tableToUpdate+ "." + colToUpdate + "<>'" + constant + "'"
+                        + " OR " + tableToUpdate+ "." + colToUpdate + " IS NULL);";
+        System.out.println("Mise a jour ->\n\t" + updateQuery);
 
         try (Statement statement = conn.createStatement()) {
             statement.executeUpdate(updateQuery);
@@ -278,20 +292,65 @@ public class Chase {
         }
     }
 
-    private static void updateTableWithConstant(Connection conn, String tableToUpdate, String colToUpdate, String constant, String wherePart) {
-        String updateQuery =
-                "UPDATE " + tableToUpdate
-                        + " SET " + colToUpdate + " = '" + constant + "'"
-                        + " " + wherePart
-                        + " AND (" + tableToUpdate+ "." + colToUpdate + "<>'" + constant + "'"
-                        + " OR " + tableToUpdate+ "." + colToUpdate + " IS NULL);";
-        System.out.println("update query const -> " + updateQuery);
+    public static boolean satisfiesEGD(Connection co, EGD egd) {
+        String cstPhi = "WHERE "; // par exemple si on a y1 = y2
+        String tablesPhi = ""; // tables utilisees dans phi
+        String colPhi = ""; // colonnes utilisees dans phi
 
-        try (Statement statement = conn.createStatement()) {
-            statement.executeUpdate(updateQuery);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        /**
+         * On construit la suite de conjonction dans le where
+         */
+        for(Dependency.Atom atomPhi: egd.phi) {
+            if(atomPhi instanceof Dependency.EqualityAtom) {
+                Dependency.EqualityAtom equalityAtom = (Dependency.EqualityAtom) atomPhi;
+                String table1Name = equalityAtom.table1.getName();
+                String table2Name = equalityAtom.table2.getName();
+                if(!tablesPhi.contains(table1Name)) tablesPhi += equalityAtom.table1.getName() + ",";
+                if(!tablesPhi.contains(table2Name)) tablesPhi += equalityAtom.table2.getName() + ",";
+                if(equalityAtom.isConst1) {
+                    cstPhi += equalityAtom.table2.getName() + "." + equalityAtom.nomCol2 + " = '" + equalityAtom.nomCol1 + "' AND ";
+                } else if (equalityAtom.isConst2) {
+                    cstPhi += equalityAtom.table1.getName() + "." + equalityAtom.nomCol1 + " = '" + equalityAtom.nomCol2 + "' AND ";
+                } else {
+                    cstPhi += equalityAtom.table1.getName() + "." + equalityAtom.nomCol1 + " = " + equalityAtom.table2.getName() + "." + equalityAtom.nomCol2 + " AND ";
+                }
+            }
+            else {
+                Dependency.RelationalAtom relationalAtom = (Dependency.RelationalAtom) atomPhi;
+                String tableName = relationalAtom.table.getName();
+                if(!tablesPhi.contains(tableName)) tablesPhi += tableName + ",";
+                for(String col: relationalAtom.constants.keySet()) {
+                    cstPhi += relationalAtom.table.getName() + "." + col + " = '" + relationalAtom.constants.get(col) + "' AND ";
+                }
+                for(String col: relationalAtom.colonnes.keySet()) colPhi += relationalAtom.table.getName() + "." + col + ",";
+            }
         }
+        tablesPhi = tablesPhi.substring(0, tablesPhi.length() - 1); // remove last comma
+        cstPhi = cstPhi.substring(0, cstPhi.length() - 5); // remove last AND
+        colPhi = colPhi.substring(0, colPhi.length() - 1); // remove last comma
+
+        /**
+         * La deuxieme partie du where s'assure l'existence de tuples
+         * pour les colonnes utilisees dans phi
+         */
+        String wherePart = cstPhi + " AND EXISTS (SELECT " + colPhi + " FROM " + tablesPhi + " LIMIT 1)";
+
+        for(Dependency.EqualityAtom atom: egd.psi) {
+            String query = "SELECT COUNT(*) FROM " + tablesPhi + " " +
+                    wherePart + " AND " + atom.table1.getName() + "." + atom.nomCol1 + " <> " + atom.table2.getName() + "." + atom.nomCol2 + " " +
+                    "OR " + atom.table1.getName() + "." + atom.nomCol1 + " IS NULL " +
+                    "OR " + atom.table2.getName() + "." + atom.nomCol2 + " IS NULL;";
+            System.out.println("Verification finale EGD : " + query);
+            try (Statement statement = co.createStatement()) {
+                ResultSet rs = statement.executeQuery(query);
+                rs.next();
+                if(rs.getInt(1) > 0) return false;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
     }
 
     /*****************************************************
@@ -320,7 +379,7 @@ public class Chase {
                 cstPhi += atomPhi.table.getName() + "." + col + " = '" + atomPhi.constants.get(col) + "' AND ";
             }
         }
-        tablesPhi = tablesPhi.substring(0, tablesPhi.length() - 1);
+        tablesPhi = tablesPhi.substring(0, tablesPhi.length() - 1); // remove last comma
 
         for(Dependency.RelationalAtom atomPsi: tgd.psi) {
             tablesPsi += atomPsi.table.getName() + ",";
@@ -346,11 +405,10 @@ public class Chase {
             for (Dependency.RelationalAtom atomPhi : tgd.phi) {
                 String colPhi = atomPhi.order.get(order);
                 if (colPhi != null) {
-                    query += atomPhi.table.getName() + "." + colPhi;
                     for (Dependency.RelationalAtom atomPsi : tgd.psi) {
                         String colPsi = atomPsi.order.get(order);
                         if (colPsi != null) {
-                            query += " = " + atomPsi.table.getName() + "." + colPsi + " AND ";
+                            query += atomPhi.table.getName() + "." + colPhi + " = " + atomPsi.table.getName() + "." + colPsi + " AND ";
                         }
                     }
                     order--;
@@ -360,7 +418,7 @@ public class Chase {
         query = query.substring(0, query.length() - 5);
         query += ");";
 
-        System.out.println("On verifie : " + query + "\n");
+        System.out.println("\nOn verifie : " + query + "\n");
 
         try (Statement statement = conn.createStatement()) {
             statement.executeQuery(query);
@@ -405,24 +463,10 @@ public class Chase {
         return res;
     }
 
-    public static void insertTuple(Connection conn, Table table, Map<String, String> tuple) {
-        String query = "INSERT INTO " + table.getName() + " VALUES (";
-        for(String col : table.getColumns()){
-            query += "'" + tuple.get(col) + "', ";
-        }
-        query = query.substring(0, query.length() - 2); // remove last comma
-        query += ");";
-        System.out.println("-> " +query);
-        try (Statement statement = conn.createStatement()) {
-            statement.executeUpdate(query);
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
+
 
     /**
-     * 1. Pour chaque table de psi (rappel : phi et psi ne contiennent que des tables
-     * effectivement affectees par la TGD), on recupere les colonnes de la table
+     * 1. Pour chaque table de psi, on recupere les colonnes de la table
      * 2. Pour chaque colonne de la table
      *  -  si elle est impliquee dans la TGD, on lui donne la valeur de la
      *     colonne correspondante dans phi
@@ -442,6 +486,21 @@ public class Chase {
             }
             System.out.println("Nouvelle insertion");
             insertTuple(co, tablePsi, newTuple);
+        }
+    }
+
+    public static void insertTuple(Connection conn, Table table, Map<String, String> tuple) {
+        String query = "INSERT INTO " + table.getName() + " VALUES (";
+        for(String col : table.getColumns()){
+            query += "'" + tuple.get(col) + "', ";
+        }
+        query = query.substring(0, query.length() - 2); // remove last comma
+        query += ");";
+        System.out.println("\t-> " +query);
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
         }
     }
 
@@ -470,6 +529,7 @@ public class Chase {
                     if (dependency instanceof TGD) {
                         TGD tgd = (TGD) dependency;
                         if (canApply(conn, tgd)) {
+                            System.out.println("\nOn applique la TGD " + tgd);
                             /**
                              * On recupere les tuples qui ne satisfont pas la TGD
                              */
@@ -480,28 +540,51 @@ public class Chase {
                              * on ajoute les tuples
                              */
                             if (tuplesNotSatisfied.size() != 0) {// la tgd n'est pas satisfaite
-                                System.out.println("TGD NON SATISFAITE ! \nA inserer pour satisfaire la TGD = " + tuplesNotSatisfied);
+                                System.out.println("TGD NON SATISFAITE ! \nA inserer pour satisfaire la " +
+                                        "TGD = " + tuplesNotSatisfied);
                                 for (Map<String, String> toAdd : tuplesNotSatisfied) {
                                     insertTuple(conn, toAdd, correspondingCols, tgd.tablesPsi);
                                 }
                             }
+                            else {
+                                System.out.println("TGD " + tgd + " SATISFAITE !");
+                            }
+                            dependency.applied = true;
+                            applied++;
                         }
                     } else {
                         EGD egd = (EGD) dependency;
                         if (canApply(conn, egd)) {
-                            System.out.println("ON APPLIQUE L'EGD " + egd);
+                            System.out.println("\nOn applique l'EGD " + egd);
                             /**
                              * On fait directement le scan des attributs dans psi et
                              * on egalise si besoin
                              */
                             verifiesEGD(conn, egd);
+                            dependency.applied = true;
+                            applied++;
                         }
                     }
-                    dependency.applied = true;
-                    applied++;
                 }
             }
         }
-        return false;
+        for(Dependency dependency : dependencies) {
+            if (dependency instanceof TGD) {
+                TGD tgd = (TGD) dependency;
+                if (canApply(conn, tgd)) {
+                    List<Map<String, String>> tuplesNotSatisfied = verifiesTGD(conn, tgd);
+                    Map<String, String> correspondingCols = tgd.getCorrespondingColumnsFromOrder();
+                    if (tuplesNotSatisfied.size() != 0) {
+                        return false;
+                    }
+                }
+            } else {
+                EGD egd = (EGD) dependency;
+                if (canApply(conn, egd)) {
+                    satisfiesEGD(conn, egd);
+                }
+            }
+        }
+        return true;
     }
 }
